8940e863d7ba756899646e18e6724d144efcd50b
Nyes98 <111405218+Nyes98@users.noreply.github.com>
2023-04-10 21:34:21 +0900

    test

--- next/test6/src/pages/hihi.tsx
+++
@@ -1,7 +0,0 @@
-import CenterMode from "@/components/carousel/main";
-
-const hi = () => {
-  return <CenterMode />;
-};
-
-export default hi;
---
+++ next/test6/src/pages/ys.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, incrementByAmount, selectCount } from '../redux/reducers/counterSlice';
+
+const IndexPage = () => {
+    const count = useSelector(selectCount);
+    const dispatch = useDispatch();
+    const [incrementAmount, setIncrementAmount] = useState('2');
+
+    const incrementValue = Number(incrementAmount) || 0;
+
+    return (
+        <div>
+            <h1>Redux Toolkit Counter Example</h1>
+            <div>
+                <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+                    +
+                </button>
+                <span>{count}</span>
+                <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+                    -
+                </button>
+            </div>
+            <div>
+                <input
+                    aria-label="Set increment amount"
+                    value={incrementAmount}
+                    onChange={(e) => setIncrementAmount(e.target.value)}
+                />
+                <button onClick={() => dispatch(incrementByAmount(incrementValue))}>Add Amount</button>
+            </div>
+        </div>
+    );
+};
+
+export default IndexPage;
